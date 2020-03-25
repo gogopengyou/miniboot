017a37085f9653a5dab85fb4f12659eabf2460ff
yangzaiju <gogoyangzaiju@126.com>
2020-03-25 19:24:07 +0800

    jpa test.

---
+++ src/main/java/com/yangzj/miniboot/domain/entity/Person.java
@@ -0,0 +1,31 @@
+package com.yangzj.miniboot.domain.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * description:
+ *
+ * @author yangzj
+ * @date 2020/03/25
+ */
+@Entity
+@Data
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
